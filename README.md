8587a19ebcafdbdc4aaf47878d6fd3b51358d74c
Daryl Sew <darylsew@gmail.com>
2013-01-01 18:51:48 -0800

    Added some dependencies and spectrum analysis code.

--- README.md
+++ README.md
@@ -10,8 +10,12 @@ Take a music file and visualize it, let the user play around with some settings 
 How we're going to do it:
 Good question. Working on that.
 
-Resources:
-last.fm API. maybe.
-wave.py python library
+Resources/dependencies (we have a LOT of dependencies):
+last.fm API
+wavepy (sudo pip install wave)
+enable (sudo apt-get install python-enable)
+pyaudio (sudo apt-get install python-pyaudio)
+scipy/numpy (sudo apt-get install python-numpy python-scipy)
+chaco (sudo pip install chaco)
 
 We're not going to use the branch workflow until we agree on a workflow model (presumably in the next few days). For now, enjoy the proof of concept code.
--- waveproof.py
+++ waveproof.py
@@ -1,7 +1,46 @@
 import wave
 
 f = wave.open("wubwub.wav", 'r')
-
+print f.getparams()
+print f.readframes(1000)
 print f.getparams()
 
-print f.readframes(20)
+"""
+import scipy, pylab
+import wave
+import struct
+import sys
+
+def stft(data, cp, do, hop):
+    dos = int(do*cp)
+    w = scipy.kaiser(dos,12) #12 is very high for kaiser window
+    temp=[]
+    wyn=[]
+    for i in range(0, len(data)-dos, hop):
+        temp=scipy.fft(w*data[i:i+dos])
+        max=-1
+        for j in range(0, len(temp),1):
+            licz=temp[j].real**2+temp[j].imag**2
+            if( licz>max ):
+                max = licz
+                maxj = j
+        wyn.append(maxj)
+    #wyn = scipy.array([scipy.fft(w*data[i:i+dos])
+        #for i in range(0, len(data)-dos, 1)])
+    return wyn
+
+file = wave.open(sys.argv[1])
+bity = file.readframes(file.getnframes())
+data=struct.unpack('{n}h'.format(n=file.getnframes()), bity)
+file.close()
+
+cp=44100 #sampling frequency
+do=0.05 #window size
+hop = 5
+
+wyn=stft(data,cp,do,hop)
+print len(wyn)
+for i in range(0, len(wyn), 1):
+    print wyn[i]
+"""
+
