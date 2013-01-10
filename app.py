a3f1e4f1ed688d75743af767dc79472d66fe1e8c
darylsew <darylsew@gmail.com>
2013-01-10 12:57:41 -0500

    Update app.py
    Added wavfile data extraction function to index(); passed the centroids, frequencies and volumes arrays to the index.html file.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import request,Flask,render_template, url_for,redirect,request
 import urllib2,json
+import svt
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    centroids, frequencies, volumes = svt.processWav("wubwub.wav", 1)
+    return render_template("index.html",
+                            centroids=centroids,
+                            frequencies=frequencies,
+                            volumes=volumes)
 
 if __name__=="__main__":
     app.debug=True
