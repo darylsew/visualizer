3ed954f732ea738813005ac3f482ef159252e1f1
Daryl Sew <darylsew@gmail.com>
2013-01-21 06:56:21 -0800

    Update wavanalysisexample.py Added FileIO code - frequencies, volumes and centroids are all in text files. I realize now that frequencies take up way, way too much space (136MB) so I'll probably truncate the floating points and possibly pass data semi-realtime (in chunks) instead of all at once.

--- wavanalysisexample.py
+++ wavanalysisexample.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
 import random
-
+import numpy
 centroids, frequencies, volumes = svt.processWav("wubwub.wav", 1)
 """
 print centroids[1]
@@ -25,11 +25,11 @@ for i in range(len(frequencies)):
     matplotlib.pyplot.scatter(numbers, frequencies[i])
     matplotlib.pyplot.show()
 """
-
+"""
 fig = pylab.figure()
 ax = Axes3D(fig)
 
-sequence_containing_x_vals = range(0, 2050)
+sequence_containing_x_vals = range(0, 2050
 sequence_containing_y_vals = range(0, 2050)
 sequence_containing_z_vals = range(0, 2050)
 
@@ -39,3 +39,29 @@ random.shuffle(sequence_containing_z_vals)
 
 ax.scatter(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals)
 pyplot.show()
+"""
+"""
+f = open("volumes.txt", 'w')
+for item in volumes:
+    print>>f, item
+f.close()
+f = open("centroids.txt", 'w')
+for item in centroids:
+    print>>f, item
+f.close()
+f = open("frequencies.txt", 'w')
+for i in range(len(frequencies)):
+    print>>f, "Frequencies at frame"+str(i)
+    for item in frequencies[i]:
+        print>>f, item
+f.close()
+"""
+
+print "Volumes"
+print "Max: " + str(max(volumes))
+print "Avg: " + str(numpy.mean(volumes))
+print "Min: " + str(min(volumes))
+print "Centroids"
+print "Max: " + str(max(centroids))
+print "Avg: " + str(numpy.mean(centroids))
+print "Min: " + str(min(centroids))
