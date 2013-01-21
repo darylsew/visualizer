15c602f42110a5d7bf5485257e2f934361d27de2
Daryl Sew <darylsew@gmail.com>
2013-01-21 06:27:29 -0800

    Update wavanalysisexample.py Wrote 3D plot code so we can get an idea of what the frequencies should look like; can't figure out what the axes are, so I'm handing it off to someone else.

--- wavanalysisexample.py
+++ wavanalysisexample.py
@@ -1,7 +1,10 @@
 #Example of how to extract data from a wav file using the modified library we wrote
 import svt
 import matplotlib
+from matplotlib import pyplot
 import pylab
+from mpl_toolkits.mplot3d import Axes3D
+import random
 
 centroids, frequencies, volumes = svt.processWav("wubwub.wav", 1)
 """
@@ -12,7 +15,7 @@ print volumes[1]
 for i in range(0, int(len(frequencies[1])/20)):
     print frequencies[1][i]
 """
-
+"""
 numbers = []
 for i in range(len(frequencies[1])):
     numbers.append(i)
@@ -21,3 +24,18 @@ for i in range(len(frequencies[1])):
 for i in range(len(frequencies)):
     matplotlib.pyplot.scatter(numbers, frequencies[i])
     matplotlib.pyplot.show()
+"""
+
+fig = pylab.figure()
+ax = Axes3D(fig)
+
+sequence_containing_x_vals = range(0, 2050)
+sequence_containing_y_vals = range(0, 2050)
+sequence_containing_z_vals = range(0, 2050)
+
+random.shuffle(sequence_containing_x_vals)
+random.shuffle(sequence_containing_y_vals)
+random.shuffle(sequence_containing_z_vals)
+
+ax.scatter(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals)
+pyplot.show()
