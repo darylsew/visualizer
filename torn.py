abd5ee2e61fab5bcaf439496ab21de1adcc67b53
Daryl Sew <darylsew@gmail.com>
2013-01-22 21:27:36 +0000

    tornado woohoo

---
+++ torn.py
@@ -0,0 +1,9 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from app import app
+
+http_server = HTTPServer(WSGIContainer(app))
+#in theory, put the URL here and stuff works. yeeeah.
+http_server.listen(9998)
+IOLoop.instance().start()
