48ef440063e4c64fae896e2f6cd28c62e8258467
honz <honwei95@gmail.com>
2013-01-21 22:02:57 -0500

    hurr hurr hon wei wrote something that works

--- static/js/honwee1.js
+++ static/js/honwee1.js
@@ -66,44 +66,20 @@
 		renderer = new THREE.CanvasRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
-/*		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 500;
-
-        scene = new THREE.Scene();
-
-        geometry = new THREE.SphereGeometry( 200,32,16);
-        material = new THREE.MeshLambertMaterial( {color: 0x8888ff} );//THREE.MeshNormalMaterial();
-		//THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-		light = new THREE.PointLight(0xffffff);
-		light.position.set(0,250,500);
-		light.intensity=1.2;
-		light.distance = 2500;
-		scene.add(light);
-		
-		ambientLight = new THREE.AmbientLight(0x555555);
-		scene.add(ambientLight);
-        renderer = new THREE.CanvasRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-        document.body.appendChild( renderer.domElement );
-*/
     }
 	function orbsfreqjitter(){
-		var arrgh = centroids[n];
-		jumpy0 = 0;
-		jumpy1 = 0;
-		jumpy2 = 0;
+		var arrgh = frequencies[counter];
+		jumpy0 = 0.0;
+		jumpy1 = 0.0;
+		jumpy2 = 0.0;
 		for (var i = 0; i < arrgh.length; i++){
-			if(i%3 == 0){jumpy0 += arrgh[i];}
+			if((i%3) == 0){jumpy0 += arrgh[i];}
 			if(i%3 == 1){jumpy1 += arrgh[i];}
 			if(i%3 == 2){jumpy2 += arrgh[i];}
 		}
-		jumpy0 /= arrgh.length;
-		jumpy1 /= arrgh.length;
-		jumpy2 /= arrgh.length;
+		jumpy0 /= (arrgh.length/3);
+		jumpy1 /= (arrgh.length/3);
+		jumpy2 /= (arrgh.length/3);
 		return [jumpy0-.5,jumpy1-.5, jumpy2-.5];
 	}
 	function orbsreposition(){
@@ -121,10 +97,10 @@
 		orb3.position.z = whee[2];
 		orb3.rotation.z +=6*Math.PI/180;
 		centerSphere.rotation.z +=3*Math.PI/180;
-		centerSphere.scale.x = volumes[n];
-		centerSphere.scale.y = volumes[n];
-		centerSphere.scale.z = volumes[n];
-		hexString = (Math.floor(centroids[n]*16777215)).toString(16);
+		centerSphere.scale.x = volumes[counter];
+		centerSphere.scale.y = volumes[counter];
+		centerSphere.scale.z = volumes[counter];
+		hexString = (Math.floor(centroids[counter]*16777215)).toString(16);
 		centerSphere.material.color.setHex(parseInt(hexString, 16));
 		orb1.material.color.setHex(parseInt(hexString, 16));
 		orb2.material.color.setHex(parseInt(hexString, 16));
@@ -147,25 +123,7 @@
 
         // note: three.js includes requestAnimationFrame shim
         requestAnimationFrame( animate );
-/*
-//        mesh.rotation.x += 0.01;
-//        mesh.rotation.y += 0.02;
-//        mesh.rotation.z += 0.03;
-		var dtime	= Date.now() - startTime;
-//		camera.position.z = 500*Math.sin(dtime/800);//*Math.PI / 180;
-//		camera.position.x = 500*Math.cos(dtime/800);//*Math.PI / 180;
-		
-//		camera.rotation.z += 3*Math.PI/180;
-//		camera.lookAt(new THREE.Vector3(0,0,0));
-		hexString = (Math.floor(centroids[n]*16777215)).toString(16);
-		mesh.material.color.setHex(parseInt(hexString, 16));
-//		mesh.scale.x = volumes[n];
-//		mesh.scale.y = volumes[n];
-//		mesh.scale.z = volumes[n];
-		n = (n+1)%maxlen;
-//		mesh.scale = 20*Math.sin(dtime/300);
-*/
-		n = (n+1)%maxlen;
+		counter = (counter+1)%maxlen;
 		alpha = (alpha + 15)%360;
 		beta1 = (beta1 + 1.5)%360;
 //		beta2 = (beta2 + 1.5)%360;
