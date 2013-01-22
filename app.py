90cd00f99e7e1dc08a68fdfa2200d8f736602042
Peter Jasko <peter@peter-1015PN.(none)>
2013-01-22 13:26:42 -0500

    Added server side processing of all the songs but commented out as to not interfere with testing.

--- app.py
+++ app.py
@@ -2,15 +2,28 @@ from flask import request,Flask,render_template, url_for,redirect,request
 import urllib2,json
 import svt
 
-global centroids, frequencies, volumes
+global centroids, frequencies, volumes,reg1centroids, reg1frequencies, reg1volumes, reg2centroids, reg2frequencies, reg2volumes, cypruscentroids, cyprusfrequencies,cyprusvolumes, dubcentroids, dubfrequencies,dubvolumes,rivercentroids, riverfrequencies,rivervolumes,sandscentroids, sandsfrequencies, sandsvolumes,starcentroids, starfrequencies, starvolumes,supercentroids, superfrequencies, supervolumes
+
 centroids, frequencies, volumes = svt.processWav("wubwub.wav", 1)
-#print "before the routine"
+#reg1centroids, reg1frequencies, reg1volumes = svt.processWav("./static/reg1.wav", 1)
+#reg2centroids, reg2frequencies, reg2volumes = svt.processWav("./static/reg2.wav", 1)
+#cypruscentroids, cyprusfrequencies,cyprusvolumes = svt.processWav("./static/cyprus.wav", 1)
+#dubcentroids, dubfrequencies,dubvolumes = svt.processWav("./static/dubstep.wav", 1)
+#rivercentroids, riverfrequencies,rivervolumes = svt.processWav("./static/river.wav", 1)
+#sandscentroids, sandsfrequencies, sandsvolumes = svt.processWav("./static/sands.wav", 1)
+#starcentroids, starfrequencies, starvolumes = svt.processWav("./static/starstuff.wav", 1)
+#supercentroids, superfrequencies, supervolumes = svt.processWav("./static/superposition.wav", 1)
+
+
+
+
+
 app=Flask(__name__)
 
 @app.route("/")
 def index():
     #centroids, frequencies, volumes = preProcess()
-    #print "after the routine"
+
     return render_template("testing.html",
                             centroids=centroids,
                             frequencies=frequencies,
